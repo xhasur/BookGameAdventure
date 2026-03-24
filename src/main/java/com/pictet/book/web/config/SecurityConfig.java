b8a1c04569a41b77382bacc8ff3bf683ae353b3a
Andres L <>
2026-03-24 20:27:07 +0000

    It was added SpringSecurity with basic authentication

---
+++ src/main/java/com/pictet/book/web/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.pictet.book.web.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Value("${app.security.username}")
+  private String username;
+
+  @Value("${app.security.password}")
+  private String password;
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(
+            customizeRequests ->
+                customizeRequests
+                    .requestMatchers("/api/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .csrf(AbstractHttpConfigurer::disable)
+        .httpBasic(Customizer.withDefaults());
+
+    return http.build();
+  }
+
+  @Bean
+  public UserDetailsService userInMemory() {
+    UserDetails adminUser =
+        User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN").build();
+    return new InMemoryUserDetailsManager(adminUser);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
